ea31ed5568126df6ea0a0cfc7d5d56a79a054d3d
Anjaneyulu Derangula <anjaneyulu.derangula9@gmail.com>
2020-03-25 18:43:38 +0530

    build

---
+++ build/precache-manifest.3ce05840ab84b4243784094af6da45d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da4b60624e601040009ee9f460d8974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9b21bd096509531732b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "df786adcab62cef31187",
+    "url": "/static/js/2.665c7eb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.665c7eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b21bd096509531732b",
+    "url": "/static/js/main.8bb14a6b.chunk.js"
+  },
+  {
+    "revision": "b79c26a913f0f099346f",
+    "url": "/static/js/runtime-main.00096f06.js"
+  }
+]);
